d0254edd19083ebf79276fac6230efa58e031f79
Bu Shuaichen <bushuaichen@yeah.net>
2025-02-03 18:18:00 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ taboolib {
         // 安装模块
         install(Basic, Bukkit, BukkitHook, BukkitNMSUtil)
     }
-    version { taboolib = "6.2.2" }
+    version { taboolib = "6.2.3" }
 }
 
 repositories {
